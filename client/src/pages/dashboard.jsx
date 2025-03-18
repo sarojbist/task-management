0d974b5fc3fbc70d8abb53cf5486bf0d915ebe0b
sarojbist <bistsaroj24@gmail.com>
2025-03-19 01:41:44 +0530

    completed

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react"
-
+import React, { useEffect, useState } from "react";
 import authStore from "../stores/authStore";
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -7,7 +6,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 import UpdateTaskModel from "../components/updateTask";
 import DeleteTaskModel from "../components/deleteTask";
 import AddModal from "../components/addTask";
-
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
 export function DashboardPage() {
     const userInfo = authStore((state) => state.userInfo);
@@ -15,8 +14,8 @@ export function DashboardPage() {
 
     const [page, setPage] = useState(1);
     const [tasks, setTasks] = useState([]);
-
     const [filterStatus, setFilterStatus] = useState("");
+
     const fetchTasks = async () => {
         try {
             if (!token) return;
@@ -33,53 +32,77 @@ export function DashboardPage() {
             console.error("Error fetching tasks:", error);
         }
     };
+
     useEffect(() => {
         fetchTasks();
-    }, [page, filterStatus]);
+    }, [page, filterStatus,]);
+
+    const onDragEnd = (result) => {
+        if (!result.destination) return;
+
+        setTasks((prevTasks) => {
+            const reorderedTasks = [...prevTasks];
+            const [movedTask] = reorderedTasks.splice(result.source.index, 1);
+            reorderedTasks.splice(result.destination.index, 0, movedTask);
+            return reorderedTasks;
+        });
+    };
 
     return (
-        <div className="bg-gradient-to-r from-gray-500 to-gray-700 min-h-screen ">
+        <div className="bg-gradient-to-r from-gray-500 to-gray-700 min-h-screen">
             <div className="flex justify-between items-center max-w-md py-4 mx-auto">
                 <h2 className="text-xl font-semibold text-white">Welcome, {userInfo?.regName}!</h2>
-                <AddModal refresh={fetchTasks}/>
+                <AddModal refresh={fetchTasks} />
             </div>
-            <h3 className="text-3xl font-semibold text-blue-300 text-center ">Your Tasks</h3>
+            <h3 className="text-3xl font-semibold text-blue-300 text-center">Your Tasks</h3>
             <div className="w-full min-h-[80vh] mx-auto mt-6 bg-white rounded-lg shadow-lg p-10 flex flex-col justify-between">
-                <div className="grid grid-cols-2 gap-6">
-                    {tasks.map((task) => (
-                        <div key={task._id} className="p-4 bg-gray-100 rounded-md shadow flex justify-between items-center">
-                            <div>
-                                <h3 className="text-lg font-semibold text-gray-800">{task.title}</h3>
-                                <p className="text-sm text-gray-600">{task.description}</p>
-                                <span className={`text-xs font-medium px-2 py-1 rounded-lg ${task.status === "Completed" ? "bg-green-100 text-green-600"
-                                    : task.status === "Pending" ? "bg-yellow-100 text-yellow-600"
-                                        : "bg-red-100 text-red-600"
-                                    }`}>
-                                    {task.status}
-                                </span>
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId="tasksList">
+                        {(provided) => (
+                            <div {...provided.droppableProps} ref={provided.innerRef} className="grid grid-cols-2 gap-6">
+                                {tasks.map((task, index) => (
+                                    <Draggable key={task._id} draggableId={task._id} index={index}>
+                                        {(provided) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                className="p-4 bg-gray-100 rounded-md shadow flex justify-between items-center"
+                                            >
+                                                <div>
+                                                    <h3 className="text-lg font-semibold text-gray-800">{task.title} {index}</h3>
+                                                    <p className="text-sm text-gray-600">{task.description}</p>
+                                                    <span className={`text-xs font-medium px-2 py-1 rounded-lg ${task.status === "Completed" ? "bg-green-100 text-green-600"
+                                                        : task.status === "Pending" ? "bg-yellow-100 text-yellow-600"
+                                                            : "bg-red-100 text-red-600"
+                                                        }`}>
+                                                        {task.status}
+                                                    </span>
+                                                </div>
+                                                <div className="flex gap-2">
+                                                    <UpdateTaskModel task={task} refresh={fetchTasks} />
+                                                    <DeleteTaskModel task={task} refresh={fetchTasks} />
+                                                </div>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
                             </div>
-                            <div className="flex gap-2">
-                                <UpdateTaskModel task={task} refresh={fetchTasks} />
-                                <DeleteTaskModel task={task} refresh={fetchTasks} />
-                            </div>
-
-
-                        </div>
-                    ))}
-                </div>
-
+                        )}
+                    </Droppable>
+                </DragDropContext>
                 <div className="flex justify-center mt-6 space-x-4">
-
-                <select
-        className="border border-gray-300 rounded-lg px-4 py-2"
-        value={filterStatus}
-        onChange={(e) => setFilterStatus(e.target.value)}
-    >
-        <option value="">All</option>
-        <option value="Pending">Pending</option>
-        <option value="Completed">Completed</option>
-        <option value="InProgress">In Progress</option>
-    </select>
+                    <select
+                        className="border border-gray-300 rounded-lg px-4 py-2"
+                        value={filterStatus}
+                        onChange={(e) => setFilterStatus(e.target.value)}
+                    >
+                        <option value="">All</option>
+                        <option value="Pending">Pending</option>
+                        <option value="Completed">Completed</option>
+                        <option value="InProgress">In Progress</option>
+                    </select>
                     <button
                         className="bg-gray-400 text-white px-4 py-2 rounded-lg cursor-pointer"
                         onClick={() => setPage((prev) => prev - 1)}
@@ -94,10 +117,6 @@ export function DashboardPage() {
                     </button>
                 </div>
             </div>
-
-
-
-
         </div>
-    )
+    );
 }
